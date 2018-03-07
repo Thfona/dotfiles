ca1ebaf9ee0cedbff8bb4f593a88ddd19742ba87
Thiago Nascimento <31373884+Thfona@users.noreply.github.com>
2018-03-07 14:46:13 -0300

    Update lock.sh

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+# Take a screenshot
 scrot /tmp/screen.png
+
+# Blur the screenshot
 convert /tmp/screen.png -blur 0x5 /tmp/screen.png
+
+# Add the lock image
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+
+# Lock the screen
 i3lock -e -i /tmp/screen.png
+
+# Turn off screen after 30 minutes
+sleep 1800 && pgrep i3lock && xset dpms force off
